9c766491fb826101e2bb14c1b23677840e8ffed3
Eric Schnelker <mail4epseric@yahoo.com>
2019-02-08 21:29:49 -0600

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,7 @@ def gs(men, women, pref):
     matchedPairs = {}  # initialize dictionary to keep track of pairs
     s = []  # Tuples to contain pairs
     q = []
-    for m in men:
-        q.append(m)
+
     print()
     print()
     print("The pairing games have begun... Place your bets on the order in which pairs of contestants will survive.")
@@ -46,23 +45,28 @@ def gs(men, women, pref):
     print()
     print()
 
-    while q:
-        m = q.pop(0)
-        for w in pref[m]:
+    for m in men:
+        q.append(m) #queue to keep track of unpaired men - this fills the queue
+    while q: #while men in the queue
+        m = q.pop(0) #pops off first man in queue
+        for w in pref[m]: #iterates through the women in the current man's pref list
             if w not in matchedPairs:
                 matchedPairs[w] = m
                 print("Pairing " + m + " with " + w + ". Adding the happy couple to matched pairs. They have won the round.")
+                #prints to keep track of where the program is and what it's doing
                 break
             elif pref[w].index(m) < pref[w].index(matchedPairs.get(w)) and m not in matchedPairs:
-                mrDumped = matchedPairs.get(w)
+                mrDumped = matchedPairs.get(w) #if in this elif, the previous pair is broken, and mrdumped is the man from that pair
                 print(mrDumped + " is a horrible person and was dumped by " + w + ". Adding him back to the queue and pairing " + w + " with " + m)
-                matchedPairs[w] = m
-                q.append(mrDumped)
+                #prints to keep track of where the program is and what it's doing
+                matchedPairs[w] = m #new pair
+                q.append(mrDumped) #adds the main from previous pair back to queue to resend him through pairing
                 break
             print(m + " is hopeless and was rejected by " + w + ". Moving on to the next woman on his target... I mean, um... preference list.")
+            #prints to keep track of where the program is and what it's doing
     for w in women:
         s.append((matchedPairs.get(w), w))
-
+        #gives output as tuples
     return s
 
 
