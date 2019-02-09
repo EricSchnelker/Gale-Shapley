f34fb15881092213a353bf7bbb3fb75557b948d7
Eric Schnelker <mail4epseric@yahoo.com>
2019-02-09 00:00:17 -0600

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,6 @@ def gs(men, women, pref):
     print("Set.")
     print("BEGIN!!!")
     print()
-    print()
 
     for m in men:
         q.append(m) #queue to keep track of unpaired men - this fills the queue
@@ -73,18 +72,7 @@ def gs(men, women, pref):
 def gs_block(men, women, pref, blocked):
     print()
     print()
-    print("The pairing games have again began their run, although this time there's a twist! Forbidden matches! Place your bets on the order in which pairs of contestants will survive, keeping in mind the matches that are not allowed!")
-    print("...")
-    print("What's that...?")
-    print("This isn't a death match...? ")
-    print("... What the hell are you paying me for, then?")
-    print("Oh?")
-    print("They're placing bets on who will be succesfully paired and married? ")
-    print("So it IS a death match! Alright, then.")
-    print("Ready. ")
-    print("Set.")
-    print("BEGIN!!!")
-    print()
+    print("The pairing games have again begun their run, although this time there's a twist! Forbidden matches! Place your bets on the order in which pairs of contestants will survive, keeping in mind the matches that are not allowed!")
     print()
 
     matchedPairs = {}  # initialize dictionary to keep track of pairs
@@ -133,18 +121,22 @@ def gs_tie(men, women, preftie):
     matchedPairs = {}  # initialize dictionary to keep track of pairs
     s = []  # Tuples to contain pairs
     q = []
-    men_paired = []
+    men_paired = [] #Gives a way to track men paired so they don't get paired twice - better explanation below
+
     print()
     print()
-    print()
+    print("The final pairing games have now begun, and, you guessed it, there's a new twist replacing the last one. Preference ties! Place your bets on the order in which pairs of contestants will survive, keeping in mind that some contestants preferences for others are equal!")
 
     for m in men:
         q.append(m) #queue to keep track of unpaired men - this fills the queue
+        
     while q: #while men in the queue
         m = q.pop(0) #pops off first man in queue
-        for i in range(len(preftie[m])):
-            for w in preftie[m][i]:
-                if m in men_paired:
+        for i in range(len(preftie[m])): #Goes over the list of sets that corresponds to the key of the current man's name in the dictionary preftie
+            for w in preftie[m][i]: #Goes over the women in each set in the list
+                if m in men_paired: #Ran into an issue where whenever a man was paired and we would break, we couldn't break out
+                                    #of the initial for loop, so each man would try to pair with each woman. This prevents paired men
+                                    #from being paired again, as initial gale-shapley didn't account for a second for loop.
                     break
                 if w not in matchedPairs:
                     matchedPairs[w] = m
@@ -153,17 +145,18 @@ def gs_tie(men, women, preftie):
                     #prints to keep track of where the program is and what it's doing
                     break
                 else:
-                    indexNewGuy = int()
-                    indexOG = int()
-                    for i in range(len(preftie[w])):
-                        if m in preftie[w][i]:
-                            indexNewGuy = i
-                        elif matchedPairs[w] in preftie[w][i]:
-                            indexOG = i
-                        elif m in preftie[w][i] and matchedPairs[w] in preftie[w][i]:
+                    indexCurrentGuy = int() #initialize
+                    indexOG = int() #initialize
+                    for i in range(len(preftie[w])): #Goes over the list of sets that corresponds to the key of the current woman's name in the dictionary preftie
+                        if m in preftie[w][i]: #Checks to see if the current man is in the ith set in the list of sets for w.
+                            indexCurrentGuy = i #if he is, his index is now i
+                        elif matchedPairs[w] in preftie[w][i]: #Checks to see if the current man is in the ith set in the list of sets for w.
+                            indexOG = i #if he is, his index is now i
+                        elif m in preftie[w][i] and matchedPairs[w] in preftie[w][i]: #A woman can't dump her man for a man she likes equally.
                             print("Both in same preference level. Breaking...")
                             break
-                    if indexNewGuy < indexOG:
+                    if indexCurrentGuy < indexOG: #compares the indices like in initial gale-shapley. Less than is used because a lower index is 
+                                              #better than a higher index in this scenario
                         mrDumped = matchedPairs.get(w) #if in this elif, the previous pair is broken, and mrdumped is the man from that pair
                         men_paired.remove(mrDumped)
                         print(mrDumped + " is a horrible person and was dumped by " + w + ". Adding him back to the queue and pairing " + w + " with " + m)
@@ -173,8 +166,6 @@ def gs_tie(men, women, preftie):
                         q.append(mrDumped) #adds the main from previous pair back to queue to resend him through pairing
                         break
                     print(m + " is hopeless and was rejected by " + w + ". Moving on to the next woman on his target... I mean, um... preference list.")
-            else:
-                continue
 
             #prints to keep track of where the program is and what it's doing
     for w in women:
@@ -219,4 +210,3 @@ if __name__ == "__main__":
 
     match_tie = gs_tie(the_men, the_women, the_preftie)
     print(match_tie)
-
